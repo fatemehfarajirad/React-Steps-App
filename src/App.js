5f9271947b249273a15c9aea563864fefa614f46
Fatemeh <fatemeh.rad92@gmail.com>
2024-08-30 10:27:39 +0200

    Using useState

--- src/App.js
+++ src/App.js
@@ -7,21 +7,21 @@ const messages = [
 ];
 
 export default function App() {
-  const step = 1;
+  const [step, setStep] = useState(1);
 
   function handlePrevious() {
-    alert("Previous");
+    if (step > 1) setStep(step - 1);
   }
   function handleNext() {
-    alert("Next");
+    if (step < 3) setStep(step + 1);
   }
 
   return (
     <div className="steps">
       <div className="numbers">
-        <div className={`${step >= 1 ? "active" : ""}`}>1</div>
-        <div className={`${step >= 1 ? "active" : ""}`}>2</div>
-        <div className={`${step >= 1 ? "active" : ""}`}>3</div>
+        <div className={step >= 1 ? "active" : ""}>1</div>
+        <div className={step >= 1 ? "active" : ""}>2</div>
+        <div className={step >= 1 ? "active" : ""}>3</div>
       </div>
 
       <p className="message">
