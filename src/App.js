974ff796d52086b343496f362692458575e53f0c
Fatemeh <fatemeh.rad92@gmail.com>
2024-08-30 12:31:15 +0200

    Use callback function to update State based on the current value

--- src/App.js
+++ src/App.js
@@ -11,10 +11,10 @@ export default function App() {
   const [isOpen, setIsOpen] = useState(true);
 
   function handlePrevious() {
-    if (step > 1) setStep(step - 1);
+    if (step > 1) setStep((s) => s - 1);
   }
   function handleNext() {
-    if (step < 3) setStep(step + 1);
+    if (step < 3) setStep((s) => s + 1);
   }
 
   return (
@@ -27,8 +27,8 @@ export default function App() {
         <div className="steps">
           <div className="numbers">
             <div className={step >= 1 ? "active" : ""}>1</div>
-            <div className={step >= 1 ? "active" : ""}>2</div>
-            <div className={step >= 1 ? "active" : ""}>3</div>
+            <div className={step >= 2 ? "active" : ""}>2</div>
+            <div className={step >= 3 ? "active" : ""}>3</div>
           </div>
 
           <p className="message">
